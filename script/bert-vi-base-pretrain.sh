42edf9f078866f030f798d69132bd3e82f4e6950
QuangDiy <tranquangduy1810@gmail.com>
2026-02-26 11:49:24 +0700

    feat: increase NCCL timeout and disable auto-resume in training configuration

--- script/bert-vi-base-pretrain.sh
+++ script/bert-vi-base-pretrain.sh
@@ -3,5 +3,9 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
+# Increase NCCL timeout from default 10 minutes to 1 hours 
+export NCCL_ASYNC_ERROR_HANDLING=1
+export NCCL_TIMEOUT=3600
+
 # Run 
 composer --nproc 8 main.py yamls/bert/bert-vi-base-pretrain.yaml "$@"
